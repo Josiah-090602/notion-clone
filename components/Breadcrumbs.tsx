9ba9a408fc2497dbb57be3602ecb50212f259642
Josiah-090602 <viernesjosiah6@gmail.com>
2025-08-08 00:59:38 +0800

    fixed breadcrumb bug

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -7,36 +7,50 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { useDocumentData } from "react-firebase-hooks/firestore";
 import { doc } from "firebase/firestore";
 import { db } from "@/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { Fragment } from "react";
 
 function Breadcrumbs() {
   const pathname = usePathname();
   const segments = pathname.split("/");
-  const [data] = useDocumentData(doc(db, "documents", segments[2] || ""));
+  const documentId = segments[2];
+  const [data] = useDocumentData(
+    documentId ? doc(db, "documents", documentId) : null
+  );
+
+  console.log("Breadcrumbs data:", segments);
+
   return (
-    <Breadcrumb>
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/">Home</BreadcrumbLink>
-        </BreadcrumbItem>
-
-        {segments.map((segment, index) => {
-          if (!segment) return null;
-
-          const href = `/${segments.slice(1, index + 1).join("/")}`;
-
-          return (
-            <BreadcrumbItem key={index}>
-              <BreadcrumbLink href={href}>
-                {index === 2 && data ? data.title : segment}
-              </BreadcrumbLink>
+    <>
+      {pathname === "/" ? null : (
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
             </BreadcrumbItem>
-          );
-        })}
-      </BreadcrumbList>
-    </Breadcrumb>
+
+            {segments.map((segment, index) => {
+              if (!segment) return null;
+
+              const href = `/${segments.slice(1, index + 1).join("/")}`;
+
+              return (
+                <Fragment key={index}>
+                  <BreadcrumbSeparator />
+                  <BreadcrumbItem>
+                    <BreadcrumbLink href={href}>
+                      {index === 2 && data ? data.title : segment}
+                    </BreadcrumbLink>
+                  </BreadcrumbItem>
+                </Fragment>
+              );
+            })}
+          </BreadcrumbList>
+        </Breadcrumb>
+      )}
+    </>
   );
 }
 export default Breadcrumbs;
